9ddeef04e9a090afec5114edaf07dfe4be563e47
Minako <minako@eurekacomputer.jp>
2013-05-28 17:06:12 +0900

    first

---
+++ Form.Designer.cs
@@ -0,0 +1,154 @@
+﻿using System;
+
+
+namespace ST_QrPrint
+{
+    partial class FormQr
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージ リソースが破棄される場合 true、破棄されない場合は false です。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+			this.labelPrinterStatus = new System.Windows.Forms.Label();
+			this.buttonPrint = new System.Windows.Forms.Button();
+			this.buttonExit = new System.Windows.Forms.Button();
+			this.checkPrintWithoutPreview = new System.Windows.Forms.CheckBox();
+			this.textBoxPrinterState = new System.Windows.Forms.TextBox();
+			this.textBoxServerState = new System.Windows.Forms.TextBox();
+			this.label1 = new System.Windows.Forms.Label();
+			this.SuspendLayout();
+			// 
+			// labelPrinterStatus
+			// 
+			this.labelPrinterStatus.AutoSize = true;
+			this.labelPrinterStatus.Location = new System.Drawing.Point(14, 68);
+			this.labelPrinterStatus.Name = "labelPrinterStatus";
+			this.labelPrinterStatus.Size = new System.Drawing.Size(84, 15);
+			this.labelPrinterStatus.TabIndex = 0;
+			this.labelPrinterStatus.Text = "プリンターの状態";
+			// 
+			// buttonPrint
+			// 
+			this.buttonPrint.Font = new System.Drawing.Font("Meiryo UI", 12F);
+			this.buttonPrint.Location = new System.Drawing.Point(279, 152);
+			this.buttonPrint.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+			this.buttonPrint.Name = "buttonPrint";
+			this.buttonPrint.Size = new System.Drawing.Size(120, 77);
+			this.buttonPrint.TabIndex = 2;
+			this.buttonPrint.Text = "印刷する";
+			this.buttonPrint.UseVisualStyleBackColor = true;
+			this.buttonPrint.Click += new System.EventHandler(this.buttonPrint_Click);
+			// 
+			// buttonExit
+			// 
+			this.buttonExit.Font = new System.Drawing.Font("Meiryo UI", 12F);
+			this.buttonExit.Location = new System.Drawing.Point(17, 152);
+			this.buttonExit.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+			this.buttonExit.Name = "buttonExit";
+			this.buttonExit.Size = new System.Drawing.Size(114, 77);
+			this.buttonExit.TabIndex = 2;
+			this.buttonExit.Text = "終了する";
+			this.buttonExit.UseVisualStyleBackColor = true;
+			this.buttonExit.Click += new System.EventHandler(this.buttonExit_Click);
+			// 
+			// checkPrintWithoutPreview
+			// 
+			this.checkPrintWithoutPreview.AutoSize = true;
+			this.checkPrintWithoutPreview.Location = new System.Drawing.Point(279, 69);
+			this.checkPrintWithoutPreview.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+			this.checkPrintWithoutPreview.Name = "checkPrintWithoutPreview";
+			this.checkPrintWithoutPreview.Size = new System.Drawing.Size(123, 19);
+			this.checkPrintWithoutPreview.TabIndex = 3;
+			this.checkPrintWithoutPreview.Text = "プレビューせずに印刷";
+			this.checkPrintWithoutPreview.UseVisualStyleBackColor = true;
+			// 
+			// textBoxPrinterState
+			// 
+			this.textBoxPrinterState.Location = new System.Drawing.Point(17, 87);
+			this.textBoxPrinterState.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+			this.textBoxPrinterState.Multiline = true;
+			this.textBoxPrinterState.Name = "textBoxPrinterState";
+			this.textBoxPrinterState.ReadOnly = true;
+			this.textBoxPrinterState.Size = new System.Drawing.Size(382, 57);
+			this.textBoxPrinterState.TabIndex = 4;
+			// 
+			// textBoxServerState
+			// 
+			this.textBoxServerState.Location = new System.Drawing.Point(17, 30);
+			this.textBoxServerState.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+			this.textBoxServerState.Multiline = true;
+			this.textBoxServerState.Name = "textBoxServerState";
+			this.textBoxServerState.ReadOnly = true;
+			this.textBoxServerState.Size = new System.Drawing.Size(382, 34);
+			this.textBoxServerState.TabIndex = 5;
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Location = new System.Drawing.Point(14, 11);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(109, 15);
+			this.label1.TabIndex = 6;
+			this.label1.Text = "データサーバーの状態";
+			// 
+			// FormQr
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.BackColor = System.Drawing.SystemColors.Control;
+			this.ClientSize = new System.Drawing.Size(415, 245);
+			this.Controls.Add(this.label1);
+			this.Controls.Add(this.textBoxServerState);
+			this.Controls.Add(this.textBoxPrinterState);
+			this.Controls.Add(this.checkPrintWithoutPreview);
+			this.Controls.Add(this.buttonExit);
+			this.Controls.Add(this.buttonPrint);
+			this.Controls.Add(this.labelPrinterStatus);
+			this.Font = new System.Drawing.Font("Meiryo UI", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+			this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+			this.MaximizeBox = false;
+			this.Name = "FormQr";
+			this.Text = "スポーツタイムマシン　QRコード発行";
+			this.Activated += new System.EventHandler(this.FormQr_Activated);
+			this.Deactivate += new System.EventHandler(this.FormQr_Deactivate);
+			this.Load += new System.EventHandler(this.Form1_Load);
+			this.Paint += new System.Windows.Forms.PaintEventHandler(this.FormQr_Paint);
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+        }
+
+        #endregion
+
+		private System.Windows.Forms.Label labelPrinterStatus;
+        private System.Windows.Forms.Button buttonPrint;
+        private System.Windows.Forms.Button buttonExit;
+		private System.Windows.Forms.CheckBox checkPrintWithoutPreview;
+		private System.Windows.Forms.TextBox textBoxPrinterState;
+		private System.Windows.Forms.TextBox textBoxServerState;
+		private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Form.cs
@@ -0,0 +1,127 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Drawing.Printing;
+
+namespace ST_QrPrint
+{
+    public partial class FormQr : Form
+    {
+        private DotNetBarcode barcode = new DotNetBarcode();
+
+        public FormQr()
+        {
+            InitializeComponent();
+        }
+
+        private void buttonExit_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            DotNetBarcode barcode = new DotNetBarcode();
+            barcode.Type = DotNetBarcode.Types.QRCode;
+
+            font = new Font("Courier New", 10);
+
+			textBoxServerState.Text =
+				"良好";
+
+			PrintDocument pd = new PrintDocument();
+			var prn = pd.PrinterSettings.DefaultPageSettings;
+			textBoxPrinterState.Text =
+				pd.PrinterSettings.PrinterName+"\r\n"+
+				prn.PrinterResolution.X+"x"+prn.PrinterResolution.Y+"dpi\r\n"+
+				prn.PaperSize.Kind.ToString()+"\r\n";
+		}
+
+        private void FormQr_Paint(object sender, PaintEventArgs e)
+        {
+        }
+
+        Font font;
+
+        private void pd_PrintPage(object sender, PrintPageEventArgs e)
+        {
+            Debug.Print("hello");
+            Debug.Print(e.MarginBounds.Right.ToString());
+            Debug.Print(e.MarginBounds.Bottom.ToString());
+            Debug.Print(e.MarginBounds.ToString());
+
+
+			barcode.QRCopyToClipboard("ABCD", 5);
+			Image img = Clipboard.GetImage();
+			Debug.Print(img.Width.ToString());
+			Debug.Print(img.Height.ToString());
+			int qr_size = img.Width;
+            
+
+            var g = e.Graphics;
+
+			int text_width = (int)g.MeasureString("ABCD", font).Width;
+
+			// A4: 8.27in x 11.69in
+			const int cel_width  = 827/4;
+			const int cel_height = 1169/6;
+
+            for (int y=0; y<8; ++y)
+            {
+                for (int x=0; x<4; ++x)
+                {
+					int dx = x * cel_width;
+					int dy = y * cel_height;
+
+                    g.DrawRectangle(
+                        (x^y)%2==0 ? Pens.Red : Pens.Black,
+						new Rectangle(dx+2, dy+2, dx + cel_width-2, dy+cel_height-2));
+                    barcode.QRWriteBar("ABCD",
+						   dx + (cel_width-qr_size)/2,
+						   dy + 10,
+                           5, e.Graphics);
+                    g.DrawString("ABCD", font, Brushes.Black,
+                           new PointF(
+							   dx + (cel_width-text_width)/2,
+							   dy + 160));
+                }
+            }
+            e.HasMorePages = false;
+        }
+
+        private void buttonPrint_Click(object sender, EventArgs e)
+        {
+            var pd = new PrintDocument();
+            pd.PrintPage += new PrintPageEventHandler(pd_PrintPage);
+
+			if (checkPrintWithoutPreview.Checked)
+			{
+				pd.Print();
+			}
+			else
+			{
+				var ppd = new PrintPreviewDialog();
+				ppd.Document = pd;
+				ppd.ShowDialog();
+			}
+        }
+
+		private void FormQr_Activated(object sender, EventArgs e)
+		{
+			this.BackColor = SystemColors.GradientActiveCaption;
+			this.Opacity = 1.0f;
+		}
+
+		private void FormQr_Deactivate(object sender, EventArgs e)
+		{
+			this.BackColor = SystemColors.GradientInactiveCaption;
+			this.Opacity = 0.9f;
+		}
+    }
+}
